dafcfc16ad593cc0882101797ba225260ff73260
Ramsés Ortiz Castro <Ramses.ortiz36@gmail.com>
2022-10-25 12:30:01 -0700

    comprueba formato de JSON

--- JavaScrip_Hola_Mundo.js
+++ JavaScrip_Hola_Mundo.js
@@ -1,1 +1,7 @@
-console.log("¡HolaMundo!");
+var JSON= {"Nombre":"Ramses","Edad":34,"Nacionalidad":"Mexicano"}
+var dato= '{"Nombre":"Ramses","Edad":34,"Nacionalidad":"Mexicano"}'
+console.log("¡Hola Mundo!");
+var obj = JSON.parse(dato);
+console.log(obj.Nombre);
+
+
